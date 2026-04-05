0173a0df644137e7ad232957da1283cc9893fa4d
a-neun <153486449+eunhye-ahn@users.noreply.github.com>
2026-04-06 06:32:16 +0900

    docker세팅

---
+++ server/src/main/java/com/esstudy/jncardsearch/JncardsearchApplication.java
@@ -0,0 +1,13 @@
+package com.esstudy.jncardsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JncardsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JncardsearchApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/esstudy/jncardsearch/JncardsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esstudy.jncardsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JncardsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
